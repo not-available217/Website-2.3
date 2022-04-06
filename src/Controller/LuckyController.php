1ebf4c7c9f800c7f4ebd92f1a9ecb351f4e4efcf
not-available217 <you@example.com>
2022-04-06 15:22:09 +0000

    routes

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,16 +2,27 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class LuckyController
+class LuckyController extends AbstractController
 {
-    public function number(): Response
+    // public function number(): Response
+    // {
+    //     $number = random_int(0, 100);
+
+    //     return new Response(
+    //         '<html><body>Lucky number: '.$number.'</body></html>'
+    //     );
+    // }
+    
+    #[Route('/lucky/number', name: 'action')]
+    public function number() : Response
     {
         $number = random_int(0, 100);
-
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number
+        ]);
     }
 }
